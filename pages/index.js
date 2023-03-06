cb400d6af401b74b216338eebce5915a08f6c6d9
Ralf Siewert <ralf.siewert@actyvyst.com>
2023-03-06 13:06:59 +0100

    challenge start

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-export default function HomePage() {
+import { useEffect, useState } from "react";
+import { places } from "../data/places";
+import PlaceCard from "../components/PlaceCard";
+import Link from "next/link";
+
+export default function HomePage({ isLarge }) {
+  const [randomPlace, setRandomPlace] = useState(null);
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * places.length);
+    setRandomPlace(places[randomIndex]);
+  }, []);
+  if (!randomPlace) {
+    return null;
+  }
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <h2>Homepage</h2>
+      <h3>Place of the week</h3>
+      <PlaceCard place={randomPlace} isLarge={isLarge} />
+      <Link href="/places">See all places</Link>
+    </>
   );
 }
